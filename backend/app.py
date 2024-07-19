fc4c811bb3addfc2aac5b9b66bf821b880b07cba
Антон Ветров <55150813+Anton554@users.noreply.github.com>
2024-07-20 00:53:33 +0300

    изменен docker

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,15 @@
 from flask import Flask, send_from_directory
 import os
 
-app = Flask(__name__, static_folder='../frontend/build')  # Использование абсолютного пути для ясности
+app = Flask(__name__, static_folder='/app/frontend/build')
 
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def serve(path):
     full_path = os.path.join(app.static_folder, path)
+    print("Requested path:", path)
+    print("Full path:", full_path)
     if path and os.path.exists(full_path):
         return send_from_directory(app.static_folder, path)
     else:
